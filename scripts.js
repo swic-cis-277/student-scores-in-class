1c4027edd110bef80b9f8b62f9cafc973e5b776c
Manav Misra <manavm@visionwebsoft.com>
2020-11-19 20:35:10 -0600

    :construction: Log data from api

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+import api from "./api.js";
 import createTable from "./lib.js";
+
+(async () => {
+  try {
+    const data = await api.index();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
