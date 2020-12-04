651f850a2183e5ee040b3d291fee6bb7bd485e89
Manav Misra <manavm@visionwebsoft.com>
2020-12-03 19:07:06 -0600

    Render table from data

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
 import api from "./api.js";
-import createTable from "./lib.js";
+import { createTable, displayTable } from "./lib.js";
 
 (async () => {
-  try {
-    const data = await api.index();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+  const data = await api.index();
+  createTable(data);
+  displayTable();
 })();
