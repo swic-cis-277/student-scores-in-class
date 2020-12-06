829727db33da8f2aa8a31a610a833b17a008c855
Manav Misra <manavm@visionwebsoft.com>
2020-12-05 18:52:56 -0600

    :children_crossing: Add 'grade color coding'

--- components/Main/Table/TBody/TRow.js
+++ components/Main/Table/TBody/TRow.js
@@ -1,5 +1,21 @@
+const assignGrade = (score) => {
+  if (score >= 90) {
+    return "A";
+  }
+  if (score >= 80) {
+    return "B";
+  }
+  if (score >= 70) {
+    return "C";
+  }
+  if (score >= 60) {
+    return "D";
+  }
+  return "F";
+};
+
 export default ({ id, name, score }) => `
-  <tr>
+  <tr data-grade=${assignGrade(score)}>
     <td>${id}</td>
     <td>${name}</td>
     <td>${score}</td>
